12e6436d6508f31285b2de291be699776a450777
WN-FO <will@finallyoffle.com>
2025-04-12 12:06:00 -0700

    Update fluent-ffmpeg type definitions with setFfmpegPath method

--- types/fluent-ffmpeg.d.ts
+++ types/fluent-ffmpeg.d.ts
@@ -24,8 +24,14 @@ declare module 'fluent-ffmpeg' {
     addOutput(output: string): this;
     mergeToFile(output: string, tempDir?: string): this;
     ffprobe(callback: (err: Error | null, data?: any) => void): void;
+    setStartTime(time: number): this;
+    setDuration(duration: number): this;
   }
 
   function ffmpeg(input?: string): FfmpegCommand;
+  namespace ffmpeg {
+    function setFfmpegPath(path: string): void;
+    function ffprobe(file: string, callback: (err: Error | null, data?: any) => void): void;
+  }
   export = ffmpeg;
 } 
