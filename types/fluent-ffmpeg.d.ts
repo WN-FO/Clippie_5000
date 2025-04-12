6d077f613b4253dbfb702fd1eee0aa12663ee60f
WN-FO <will@finallyoffle.com>
2025-04-12 11:56:04 -0700

    Add custom type declarations for fluent-ffmpeg

---
+++ types/fluent-ffmpeg.d.ts
@@ -0,0 +1,30 @@
+declare module 'fluent-ffmpeg' {
+  interface FfmpegCommand {
+    input(input: string): this;
+    output(output: string): this;
+    on(event: string, callback: (err: Error | null, data?: any) => void): this;
+    run(): this;
+    save(outputPath: string): this;
+    duration(duration: number): this;
+    seekInput(time: number): this;
+    seek(time: number): this;
+    format(format: string): this;
+    size(size: string): this;
+    fps(fps: number): this;
+    videoBitrate(bitrate: string): this;
+    audioBitrate(bitrate: string): this;
+    audioChannels(channels: number): this;
+    audioFrequency(freq: number): this;
+    videoCodec(codec: string): this;
+    audioCodec(codec: string): this;
+    outputOptions(options: string | string[]): this;
+    complexFilter(filters: string | string[]): this;
+    addInput(input: string): this;
+    addOutput(output: string): this;
+    mergeToFile(output: string, tempDir?: string): this;
+    ffprobe(callback: (err: Error | null, data?: any) => void): void;
+  }
+
+  function ffmpeg(input?: string): FfmpegCommand;
+  export = ffmpeg;
+} 
