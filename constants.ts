34f6f2347e2419ce6f695f2ef0099c30f3edd3cc
WN-FO <will@finallyoffle.com>
2025-04-14 09:49:08 -0700

    chore: add environment debug logging

--- constants.ts
+++ constants.ts
@@ -1,2 +1,9 @@
 export const MAX_FREE_COUNTS = 5;
 export const APP_VERSION = "1.0.0";
+
+// Debug helper
+console.log('Environment check:', {
+  hasDatabase: !!process.env.DATABASE_URL,
+  hasSupabase: !!process.env.NEXT_PUBLIC_SUPABASE_URL,
+  hasStripe: !!process.env.STRIPE_SECRET_KEY
+});
